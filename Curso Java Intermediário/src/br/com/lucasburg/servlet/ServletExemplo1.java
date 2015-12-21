f5d4c5004cf176f686d4eee18c6d0ca9a5597158
Lucas <Lucas@192.168.1.6>
2015-12-21 17:39:23 -0200

    Estudo de Servlet e seus métodos

--- Curso Java Intermediário/src/br/com/lucasburg/servlet/ServletExemplo1.java
+++ Curso Java Intermediário/src/br/com/lucasburg/servlet/ServletExemplo1.java
@@ -14,7 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletExemplo1")
 public class ServletExemplo1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	private String mensagem;
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,14 +30,24 @@ public class ServletExemplo1 extends HttpServlet {
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+		mensagem = config.getInitParameter("mensagem");
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String nome = request.getParameter("nome");
+		
+		response.getWriter().write("<html>");
+		response.getWriter().write("<head>");
+		response.getWriter().write("<title>Teste Servlet</title>");
+		response.getWriter().write("</head>");
+		response.getWriter().write("<body>");
+		response.getWriter().write("<h1>" + mensagem + "</h1>");
+		response.getWriter().write("<p>Olá, " + nome + "</p>");
+		response.getWriter().write("</body>");
+		response.getWriter().write("</html>");
 	}
 
 }
